f34651e956d8b30e450f488d028acfdea499db48
yegor <yegor.shakh@gmail.com>
2016-03-08 22:31:54 +0200

    Task completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## $set() - stores a matrix in this envirement
+## $get() - loads the matrix from this envirement
+## $setsolve() - stores an inverted matrix to this envirement
+## $getsolve() - loads the matrix from this envirement
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  
+  # create a list of functions
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Load a stored inverted matrix or invert if it has not been stored and return
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
+}
+
+
+## Generate a square matrix with size of 'x'
+
+generateMatrix <- function(x) {
+  A <- stats::rnorm(x*x)
+  dim(A) <- c(x,x)
+  return(A)
 }
